8355d2e2545e4bd08d0a43b1a5932b2021af7b52
Simon Buysse <buysse.simon@gmail.com>
2019-12-18 22:31:01 +0100

    setup dev environment

--- src/fire.ts
+++ src/fire.ts
@@ -3,20 +3,16 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCmJ281SMzN33Dla22JRYM9fgKi0nj3Qs8",
-    authDomain: "ons-boodschappenlijstje.firebaseapp.com",
-    databaseURL: "https://ons-boodschappenlijstje.firebaseio.com",
-    projectId: "ons-boodschappenlijstje",
-    storageBucket: "ons-boodschappenlijstje.appspot.com",
-    messagingSenderId: "460723363652"
+    apiKey: process.env['REACT_APP_FIREBASE_API_KEY'],
+    authDomain: process.env['REACT_APP_FIREBASE_AUTH_DOMAIN'],
+    databaseURL: process.env['REACT_APP_FIREBASE_DATABASE_URL'],
+    projectId: process.env['REACT_APP_FIREBASE_PROJECT_ID'],
+    storageBucket: process.env['REACT_APP_FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId:  process.env['REACT_APP_FIREBASE_MESSAGING_SENDER_ID'],
 };
 
 export default firebase.initializeApp(config);
 
-const settings = {
-    timestampsInSnapshots: true
-};
-firebase.firestore().settings(settings);
 firebase.firestore().enablePersistence().catch((err) => {
     if (err.code === 'failed-precondition') {
         console.log('You have opened multiple tabs with the ourgroceries app, only the first will work properly offline');
